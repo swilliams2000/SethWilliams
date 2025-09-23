2a35aef02de9ab48040206c233c0396a7acd963f
Seth Williams <120819810+swilliams2000@users.noreply.github.com>
2025-09-23 17:39:53 -0400

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -118,14 +118,13 @@ export const SITE_CONTENT: SiteContent = {
 
     image: "/Seth-Big.jpg",
     bullets: [
-    "Full-Stack Development",
-    "DevOps & Infrastructure",
-    "OOP & DSA",
-    "Cloud Computing",
-    "Python for ML",
-    "C#",
-  ],
-},
+      "Full-Stack Development",
+      "DevOps & Infrastructure",
+      "OOP & DSA",
+      "Cloud Computing",
+      "Python for ML",
+      "C#",
+    ],
   },
 };
 
